bf82d3d3b5f322b2298476a8f05903283db866d6
BGK <gorkemkizilok@gmail.com>
2018-07-11 10:37:08 +0300

    Add project files.

---
+++ valuetobinary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace valuetobinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
