62dbcc3a0d9c4d106590b08ce18836e5580b300d
BGK <gorkemkizilok@gmail.com>
2018-07-11 20:34:51 +0300

    Program.cs added

--- valuetobinary/Program.cs
+++ valuetobinary/Program.cs
@@ -6,7 +6,64 @@ namespace valuetobinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                int sayi = 1;
+                Console.Write("Enter Value: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                string def;
+                def = "";
+                while (sayi > 0)
+                {
+                    if (sayi % 2 == 0)
+                    {
+                        def += "0";
+                    }
+                    else if (sayi % 2 == 1)
+                    {
+                        def += "1";
+                    }
+                    sayi = sayi / 2;
+                }
+
+                for (int i = def.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(def[i]);
+                }
+                Console.WriteLine();
+            } while (true);
+
+
+            //string def;
+            //int sayi;
+            //for (int j = 1; j <= 1000; j++)
+            //{
+            //    sayi = j;
+            //    def = "";
+            //    while (sayi >= 0)
+            //    {
+            //        if (sayi % 2 == 0)
+            //        {
+            //            def += "0";
+            //        }
+            //        else if (sayi % 2 == 1)
+            //        {
+            //            def += "1";
+            //        }
+            //        sayi = sayi / 2;
+            //        if (sayi == 0)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    Console.Write(j+" = ");
+            //    for (int i = def.Length-1; i >= 0; i--)
+            //    {                    
+            //        Console.Write(def[i]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            Console.Read();
         }
     }
 }
